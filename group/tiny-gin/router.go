a4c663346dcf689550c8dc9a8080eda97b9366e1
许继元 <490601115@qq.com>
2022-05-11 19:29:54 +0800

    feat: 路由模块

---
+++ group/tiny-gin/router.go
@@ -0,0 +1,100 @@
+/*
+-------------------------------------
+# @Time    : 2022/5/11 14:46:37
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : router.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package tiny_gin
+
+import (
+	"net/http"
+	"strings"
+)
+
+type router struct {
+	roots    map[string]*node
+	handlers map[string]HandlerFunc
+}
+
+func newRouter() *router {
+	return &router{
+		roots:    make(map[string]*node),
+		handlers: make(map[string]HandlerFunc),
+	}
+}
+
+// parsePattern 解析路由
+func parsePattern(pattern string) []string {
+	vs := strings.Split(pattern, "/")
+	var parts []string
+	for _, item := range vs {
+		if item != "" {
+			parts = append(parts, item)
+			// 只允许一个 *
+			if item[0] == '*' {
+				break
+			}
+		}
+	}
+	return parts
+}
+
+func (r *router) addRoute(method, pattern string, handler HandlerFunc) {
+	parts := parsePattern(pattern)
+	key := method + "-" + pattern
+	_, ok := r.roots[method]
+	if !ok {
+		r.roots[method] = &node{}
+	}
+	r.roots[method].insert(pattern, parts, 0)
+	r.handlers[key] = handler
+}
+
+func (r *router) getRoute(method, path string) (*node, map[string]string) {
+	searchParts := parsePattern(path)
+	params := make(map[string]string)
+	root, ok := r.roots[method]
+	if !ok {
+		return nil, nil
+	}
+	n := root.search(searchParts, 0)
+	if n != nil {
+		parts := parsePattern(n.pattern)
+		for idx, part := range parts {
+			if part[0] == ':' {
+				params[part[1:]] = searchParts[idx]
+			}
+			if part[0] == '*' && len(part) > 1 {
+				params[part[1:]] = strings.Join(searchParts[idx:], "/")
+				break
+			}
+		}
+		return n, params
+	}
+	return nil, nil
+}
+
+func (r *router) getRoutes(method string) []*node {
+	root, ok := r.roots[method]
+	if !ok {
+		return nil
+	}
+	var nodes []*node
+	root.travel(&nodes)
+	return nodes
+}
+
+func (r *router) handle(c *Context) {
+	n, params := r.getRoute(c.Method, c.Path)
+	if n != nil {
+		c.Params = params
+		key := c.Method + "-" + n.pattern
+		r.handlers[key](c)
+	} else {
+		c.String(http.StatusNotFound, "404 NOT FOUND: %s\n", c.Path)
+	}
+}
