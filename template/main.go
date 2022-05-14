6d2733c69f1d94e9da5aae15c30b6c0b7fd5d28c
许继元 <490601115@qq.com>
2022-05-14 18:40:12 +0800

    feat: 框架使用样例

---
+++ template/main.go
@@ -0,0 +1,58 @@
+/*
+-------------------------------------
+# @Time    : 2022/5/14 1:17:25
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : main.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import (
+	"TinyGin/template/tinyweb"
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+)
+
+type student struct {
+	Name string
+	Age  int8
+}
+
+func FormatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
+}
+
+func main() {
+	r := tinyweb.New()
+	r.Use(tinyweb.Logger())
+	r.SetFuncMap(template.FuncMap{
+		"FormatAsDate": FormatAsDate,
+	})
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./static")
+
+	stu1 := &student{Name: "Giyn", Age: 22}
+	stu2 := &student{Name: "Jack", Age: 20}
+	r.GET("/", func(c *tinyweb.Context) {
+		c.HTML(http.StatusOK, "css.tmpl", nil)
+	})
+	r.GET("/students", func(c *tinyweb.Context) {
+		c.HTML(http.StatusOK, "arr.tmpl", tinyweb.H{
+			"title":  "TinyWeb",
+			"stuArr": [2]*student{stu1, stu2},
+		})
+	})
+	r.GET("/date", func(c *tinyweb.Context) {
+		c.HTML(http.StatusOK, "custom_func.tmpl", tinyweb.H{
+			"title": "TinyWeb",
+			"now":   time.Date(2022, 5, 14, 0, 0, 0, 0, time.UTC),
+		})
+	})
+	r.Run(":9999")
+}
